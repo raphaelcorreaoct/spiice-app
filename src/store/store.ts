c60126dacfd2cfd76aba973026fe3763be7bcfae
Raphael Correa <raphael.correa@bennu.com.br>
2022-08-17 19:16:25 -0300

    feature: Install and configure redux

---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import {createStore, applyMiddleware, Store} from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk';
+import rootReducer, {RootState} from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  timeout: 0,
+  whitelist: [],
+  transforms: [],
+};
+
+const persistedReducer = persistReducer<RootState>(persistConfig, rootReducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+/* eslint-enable */
+export const persistor = persistStore(store);
+export default store as Store<RootState>;
