311eef7c7e553070b0754a8272c1827864ce7fd7
Raphael Correa <raphael.correa@bennu.com.br>
2022-08-19 00:39:00 -0300

    feature: logout

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,10 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import auth from '@react-native-firebase/auth';
 
 export default function HomeScreen() {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button
+        title="Logout"
+        onPress={() => {
+          auth()
+            .signOut()
+            .then(() => console.log('User signed out!'));
+        }}
+      />
     </View>
   );
 }
